ff4f0e8433188c4b8955d8fde25c3f485bdcbc40
NicoloMartiradonna <116425882+NicoloMartiradonna@users.noreply.github.com>
2024-05-02 10:00:26 +0200

    Add files via upload

---
+++ film_db/connect.php
@@ -0,0 +1,26 @@
+<?php
+$host = '127.0.0.1';
+$dbname = 'carpooling';
+$username = 'root';
+$password = '';
+
+$mysqli = mysqli_connect($host, $username, $password, $dbname);
+
+if ($mysqli->connect_errno) {
+    die("Connessione fallita: " . $mysqli->connect_error);
+}
+
+
+
+$query = "INSERT INTO regista (nome, cognome, data_nascita) VALUES ('Mario', 'Rossi', '20-09-1992')";
+
+if ($conn->query($query) === TRUE) {
+  echo "Record inserito con successo";
+} else {
+  echo "Errore: " . $query . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+?>
